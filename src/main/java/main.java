d151a98e3f435f986a0b4b07180ce37630c73978
Buccatta <chickenwingcandy23@gmail.com>
2021-10-11 11:10:59 -0400

    First commit for problems 1- 7

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        sDrills sd = new sDrills();
+
+        sd.writeName();
+
+    }
+}
---
+++ src/main/java/sDrills.java
@@ -0,0 +1,23 @@
+public class sDrills {
+
+    private String jeff ="";
+
+    private String jane = "Rhonda";
+
+    private String beef = "";
+
+    private String pizza = "";
+
+    private String pork = null;
+        
+
+   public void writeName (){
+       System.out.println(jane);
+       System.out.println(beef);
+       System.out.println(pizza);
+       System.out.println(pork);
+
+   }
+
+
+}
